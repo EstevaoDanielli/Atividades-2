a86c25784e39ebcba8eff65714347593ebc3ac2d
EstevaoDanielli <86986832+EstevaoDanielli@users.noreply.github.com>
2021-07-09 11:58:35 -0300

    Create pokemon2.php

---
+++ pokemon2.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="https://img.elo7.com.br/product/original/28E57D8/pokebola-eva.jpg" type="image/x-png"/>
+
+    <style>
+        .card {
+            width: 200px;
+            height: 400px;
+            border: solid 1px #ddd;
+            position: relative;
+            float: left;
+            padding: 10px;
+        }
+
+        .card img,
+        .card h5 {
+            text-align: center;
+        }
+
+        img {
+            max-width: 100%;
+        }
+    </style>
+
+    <title>Pokemon</title>
+</head>
